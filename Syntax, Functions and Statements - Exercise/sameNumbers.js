b44c11aae87e7d08228b1dd3e2811cca80f0e9b8
mmilushev91 <milushev_1990@abv.bg>
2025-03-31 13:42:34 +0300

    Update sameNumbers.js

--- Syntax, Functions and Statements - Exercise/sameNumbers.js
+++ Syntax, Functions and Statements - Exercise/sameNumbers.js
@@ -2,7 +2,7 @@ function solve(num) {
   const numString = String(num);
   const uniqueDigits = new Set(numString);
   
-  console.log(numString.length !== uniqueDigits.size)
+  console.log(uniqueDigits.size === 1)
   
   const numStringArray = numString.split("").reduce((sum, el) => sum + Number(el), 0);
   console.log(numStringArray)
